d71549b009af96fac4a92657970c58c92f736fd3
Swarna-deep0 <srj368920@gmail.com>
2025-07-20 19:13:26 +0530

    print statement

--- src/guess_game.java
+++ src/guess_game.java
@@ -10,7 +10,7 @@ class game{
     }
     public void guess(){
         Scanner sc = new Scanner(System.in);
-        System.out.print("what is your guess in game hulala : ");
+        System.out.print("what is your guess : ");
         guess = sc.nextInt();
         if (guess<0){
             System.out.println("No ! negative integer is allowed in game");
