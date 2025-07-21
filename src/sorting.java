14720b0e6fc57d27642464f01bb09ad0e7eb8862
Swarna-deep0 <srj368920@gmail.com>
2025-07-21 23:51:57 +0530

    new class

--- src/sorting.java
+++ src/sorting.java
@@ -1,5 +1,5 @@
 class sort{
-    int[]arr = {5,3,4,7,9};
+    int[]arr = {5,3,4,7,1};
     int temp;
     int[]ori;
     void original() {
@@ -11,7 +11,7 @@ class sort{
     public sort(){
         ori =  arr.clone();
         for (int i=0;i<arr.length-1;i++){
-            for (int j=0;j<arr.length-1;j++){
+            for (int j=0;j<arr.length-i-1;j++){
                 if (arr[j]>arr[j+1]){
                     temp = arr[j];
                     arr[j]=arr[j+1];
