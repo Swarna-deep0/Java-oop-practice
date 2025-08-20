f09c7d91ad427616c4ff2caacca31099578b0df8
Swarna-deep0 <srj368920@gmail.com>
2025-08-20 21:52:48 +0530

    oractice

--- src/practice_set.java
+++ src/practice_set.java
@@ -2,17 +2,15 @@ package PACKAGE_NAME;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class practice_set {
     public static void main(String[] args) {
         //to find second largest value from array
-        List<Integer> l1 = Arrays.asList(23, 90, 45, 34, 78, 12);
-        int secondlargest = l1.stream()
-                .distinct()
-                .sorted((a, b) -> b - a)
-                .skip(1)
-                .findFirst()
-                .orElseThrow();
-        System.out.println();
+        List<Integer> l1 = Arrays.asList(23, 90, 45, 45, 78, 12);
+        Stream<Integer> l2 = l1.stream()
+                .distinct();
+        l2.forEach(n -> System.out.println(n));
     }
 }
