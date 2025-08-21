57c23be67265c3fdf58be799d11c7eb02f3c2010
Swarna-deep0 <srj368920@gmail.com>
2025-08-21 20:16:03 +0530

    practice

--- src/practice_set.java
+++ src/practice_set.java
@@ -7,10 +7,19 @@ import java.util.stream.Stream;
 
 public class practice_set {
     public static void main(String[] args) {
+
         //to find second largest value from array
         List<Integer> l1 = Arrays.asList(23, 90, 45, 45, 78, 12);
-        Stream<Integer> l2 = l1.stream()
-                .distinct();
-        l2.forEach(n -> System.out.println(n));
+        Integer num = l1.stream()
+                .distinct()
+                .sorted((a,b)->b-a)
+                .skip(1)
+                .findFirst()
+                .orElse(-1);
+        
+        System.out.println(num);
+
+        // String
+
     }
 }
